e0074bdf82ed2f064161b431a3527b67b1c69869
DanE <edwardsdan001@gmail.com>
2018-01-27 15:01:14 -0500

    added methods to clean up Main

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using static System.Console;
 
 namespace Lab5
 {   
@@ -15,49 +17,57 @@ namespace Lab5
             int die2 = rnd.Next(1, x);
             return die1 + die2;
         }
+        public static void PrintResult (int rolled, int i)
+        {
+            if (rolled == 2)
+                WriteLine($"Roll {i}: Snake eyes!!!");
+
+            else if (rolled == 7 || rolled == 11)
+                WriteLine($"Roll {i}: Craps!!!");
+
+            else if (rolled == 12)
+                WriteLine($"Roll {i}: Boxcars!!!");
+
+            else
+                WriteLine($"Roll {i}: {rolled}");
+        }
     }
     class Program
     {
+        public static int Validate(string x)
+        {
+            int sides;
+            bool validInput = int.TryParse(x, out sides);
+            if (validInput == false || sides > 20 || sides <= 1)
+            {
+                WriteLine("Try entering a number between 1 and 20!");
+                Validate(ReadLine());
+            }
+            return sides;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Wanna play a game? (y/n)\t");
-            string response = Console.ReadLine();
-            if (response.ToLower() == "y" || response.ToLower() == "yes")
+            WriteLine("Wanna play a game? (y/n)");
+
+            if (Regex.Match(ReadLine().ToLower(), @"^(y|yes)$").Success)
             {
                 int i = 1;
                 while (i <= int.MaxValue)
                 {
-                    Console.WriteLine("How many sides should each die have? (up to 20)");
+                    WriteLine("How many sides should each die have? (up to 20)");
+                    int rolled = DiceRollerApp.RollDice(Validate(ReadLine()));
 
-                    int sides;
-                    bool Validate = int.TryParse(Console.ReadLine(), out sides);
-                    if (Validate == false || sides > 20 || sides <= 1)
-                    {
-                        Console.WriteLine("Try entering a number between 1 and 20!");
-                        continue;
-                    }
-                    
-                    if (DiceRollerApp.RollDice(sides) == 2)
-                        Console.WriteLine($"Roll {i}: Snake eyes!!!");
+                    DiceRollerApp.PrintResult(rolled, i);
 
-                    else if (DiceRollerApp.RollDice(sides) == 7 || DiceRollerApp.RollDice(sides) == 11)
-                        Console.WriteLine($"Roll {i}: Craps!!!");
-
-                    else if (DiceRollerApp.RollDice(sides) == 12)
-                        Console.WriteLine($"Roll {i}: Boxcars!!!");
-
-                    else
-                        Console.WriteLine($"Roll {i}: {DiceRollerApp.RollDice(sides)}");
-
-                    Console.WriteLine("Wanna play again? (y/n)");
-                    string yesOrNo = Console.ReadLine();
-                    if (yesOrNo.ToLower() == "y" || yesOrNo.ToLower() == "yes")
+                    WriteLine("Wanna play again? (y/n)");
+                    if (Regex.Match(ReadLine().ToLower(), "^(y|yes)$").Success)
                         i++;
                     else
                         break;
                 }
             }
-            Console.WriteLine("Goodbye!");
+            WriteLine("Goodbye!");
         }
     }
 }
